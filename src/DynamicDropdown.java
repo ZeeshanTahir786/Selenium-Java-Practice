18ad663f1c015092af04bff68e45b38b443e95c6
Hafiz ZeeShan <65600657+ZeeshanTahir786@users.noreply.github.com>
2022-07-14 19:42:16 +0500

    Update DynamicDropdown.java

--- src/DynamicDropdown.java
+++ src/DynamicDropdown.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class DynamicDropdown {
 
@@ -10,17 +11,27 @@ public class DynamicDropdown {
 		
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise");
-		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
 		
+		Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+		Assert.assertEquals(driver.findElements(By.cssSelector("input[type='checkbox']")).size(), 6);
+		
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
 //		driver.findElement(By.xpath("//a[@value='MAA']")).click();
 //		Thread.sleep(2000);
 //		driver.findElement(By.xpath("(//a[@value='BHO'])[2]")).click();
 		
 //		If someone don't like indexing approach then try Another way:
+		
+
 		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='MAA']")).click(); // parent to child traversal
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='BHO']")).click();
 		
+		driver.close();
 	}
 
 }
