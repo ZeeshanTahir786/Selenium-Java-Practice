03b26bb9f49661effe85eeb61ef8863484527f41
Hafiz ZeeShan <65600657+ZeeshanTahir786@users.noreply.github.com>
2022-07-15 16:30:51 +0500

    Create E2E.java

---
+++ src/E2E.java
@@ -0,0 +1,59 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+public class E2E {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "/Users/hafizzeeshan/Downloads/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise");
+		
+		Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+		Assert.assertEquals(driver.findElements(By.cssSelector("input[type='checkbox']")).size(), 6);
+		
+		
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='MAA']")).click(); // parent to child traversal
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='BHO']")).click();
+//		Thread.sleep(
+		driver.findElement(By.cssSelector(".ui-state-default.ui-state-highlight")).click();
+
+		System.out.println(driver.findElement(By.id("Div1")).getAttribute("style").contains("0.5"));
+		if(driver.findElement(By.id("Div1")).getAttribute("style").contains("0.5")) {
+			Assert.assertTrue(true);
+		}else {
+			Assert.assertTrue(false);
+		}
+		
+		
+		
+		driver.findElement(By.className("paxinfo")).click();
+		Thread.sleep(1000);
+		for (int i = 1; i < 5; i++) {
+			driver.findElement(By.xpath("//div[@id='divAdult']//div[@class='ad-row-right']/span[3]")).click();
+		}
+		driver.findElement(By.id("btnclosepaxoption")).click();
+		
+		WebElement staticDropDown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+		
+		Select select = new Select(staticDropDown);
+		
+		select.selectByIndex(3);
+		System.out.println(select.getFirstSelectedOption().getText());
+		
+		driver.findElement(By.cssSelector("input[type='submit']")).click();
+	}
+
+}
