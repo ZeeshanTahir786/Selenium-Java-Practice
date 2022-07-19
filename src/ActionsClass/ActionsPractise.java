3db05313da0d9d7086667d0b8f1655357543d086
Hafiz ZeeShan <65600657+ZeeshanTahir786@users.noreply.github.com>
2022-07-19 11:26:35 +0500

    windowHandles

--- src/ActionsClass/ActionsPractise.java
+++ src/ActionsClass/ActionsPractise.java
@@ -1,7 +1,9 @@
 package ActionsClass;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -13,9 +15,14 @@ public class ActionsPractise {
 
 		driver.manage().window().maximize();
 		driver.get("https://amazon.com");
-		
+
 		Actions action = new Actions(driver);
-		action.moveToElement(driver.findElement(By.xpath("//a[@id='nav-link-accountList']"))).build().perform();
+
+		action.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT)
+				.sendKeys("hello").doubleClick().build().perform();
+
+		WebElement move = driver.findElement(By.xpath("//a[@id='nav-link-accountList']"));
+		action.moveToElement(move).contextClick().build().perform();
 
 	}
 
