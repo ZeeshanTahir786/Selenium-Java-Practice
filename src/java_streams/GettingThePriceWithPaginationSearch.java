1ecdfb1201b8b0f12a550d406640280770b1b629
Hafiz ZeeShan <65600657+ZeeshanTahir786@users.noreply.github.com>
2022-07-22 16:23:36 +0500

    GettingThePriceWithPaginationSearch

--- src/java_streams/GettingThePriceWithPaginationSearch.java
+++ src/java_streams/GettingThePriceWithPaginationSearch.java
@@ -8,8 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GettingPriceOfElement {
-//	Get the price of specific product name
+public class GettingThePriceWithPaginationSearch {
+
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "/Users/hafizzeeshan/Downloads/chromedriver");
 		WebDriver driver = new ChromeDriver();
@@ -17,14 +17,21 @@ public class GettingPriceOfElement {
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
 
-//		CAPTURE All elements into list
-		List<WebElement> elementsList = driver.findElements(By.xpath("//tbody/tr/td[1]"));
+		driver.findElement(By.xpath("//tr/th[1]")).click();
 
-		List<String> price = elementsList.stream().filter(a -> a.getText().contains("Rice")).map(s -> getPrice(s))
-				.collect(Collectors.toList());
+		List<String> price;
+		do {
+			List<WebElement> elementsList = driver.findElements(By.xpath("//tbody/tr/td[1]"));
 
-		price.forEach(s -> System.out.println(s));
+			price = elementsList.stream().filter(a -> a.getText().contains("Orange")).map(s -> getPrice(s))
+					.collect(Collectors.toList());
 
+			price.forEach(s -> System.out.println(s));
+			
+			if(price.size() < 1) {
+				driver.findElement(By.cssSelector("a[aria-label='Next']")).click();
+			}
+		} while (price.size() < 1);
 	}
 
 	private static String getPrice(WebElement s) {
